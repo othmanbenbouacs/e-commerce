7cefe03d5e2c89a26de36147039a326c4691b34d
othmanbenbouacs <othmanbenboucs50@gmail.com>
2020-04-16 08:59:44 -0400

    adding and styling the menu-items component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Homepage } from "./pages/Homepage/Homepage.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
